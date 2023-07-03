243dbcd52726686d840cb2e89838da4c7fcf52dd
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 00:48:40 +0800

    Set Fan1 Details

--- TestFan.py
+++ TestFan.py
@@ -13,6 +13,12 @@ user_radius_one = input(int("Enter the radius of the fan: "))
 user_color_one = input(str("Enter the color of the fan: "))
 user_power_one = input("Is the fan on/off: ")
 
+# Set Fan1 Details 
+fan_one.set_speed(user_speed_one)
+fan_one.set_speed(user_radius_one)
+fan_one.set_speed(user_color_one)
+fan_one.set_speed(user_power_one)
+
 # Ask the user for Fan2 Details
 user_speed_two = input("Enter the speed of the fan: ")
 user_radius_two = input(int("Enter the radius of the fan: "))
