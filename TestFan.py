d04d143d0198cbedb98edce15e19b5fc6f2a04c9
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 00:21:53 +0800

    Create two instances of the class

--- TestFan.py
+++ TestFan.py
@@ -3,19 +3,9 @@
 # 03/07/2023
 # The Fan Class
 
-# Pseudocode
-# Create a class named Fan
-# Initialize the fan object
-# Method to get the current speed of the fan 
-# Method to set the speed of the fan
-# Method to turn on the fan
-# Method to turn off the fan
-# Method to get the radius of the fan 
-# Method to set the radius of the fan
-# Method to get the color of the fan 
-# Method to set the color of the fan
-# Method to display the Output
 # Create two seperate instances of the Fan Class
+fan_one = ()
+fan_two = ()
 # Call the methods in the main file
 # Display the Output
 
