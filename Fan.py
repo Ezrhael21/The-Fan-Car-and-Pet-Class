94b9668c7a27bb30ee760916b134c74f7c9a61e5
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 00:12:36 +0800

    Change the variable and methods to private

--- Fan.py
+++ Fan.py
@@ -2,42 +2,42 @@
 class Fan:
     # Initialize the fan object
     def __init__(self, speed = 3, radius = 5, color = "blue", power = False):
-        self.speed = speed
-        self.radius = radius
-        self.color = color
-        self.power = power
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__power = power
     
     # Method to get the current speed of the fan 
     def get_speed (self):
-        return self.speed
+        return self.__speed
     
     # Method to set the speed of the fan
     def set_speed (self, speed):
-        self.speed = speed
+        self.__speed = speed
 
     # Method to get the radius of the fan 
     def get_radius (self):
-        return self.radius
+        return self.__radius
     
     # Method to set the radius of the fan
     def set_radius (self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     # Method to get the color of the fan 
     def get_color (self):
-        return self.color
+        return self.__color
     
     # Method to set the color of the fan
     def set_color (self, color):
-        self.color = color
+        self.__color = color
 
     # Method to turn on the fan
     def turn_on (self):
-        self.on = True
+        self.__on = True
     
     # Method to turn off the fan
     def turn_off(self):
-        self.on = False
+        self.__on = False
     
     
 
