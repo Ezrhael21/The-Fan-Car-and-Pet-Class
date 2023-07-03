2c05c401afb2308d93dc868c2df4e02597ad46aa
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 00:29:57 +0800

    Ask the user for Fan2 Details

--- TestFan.py
+++ TestFan.py
@@ -13,6 +13,12 @@ user_radius_one = input(int("Enter the radius of the fan: "))
 user_color_one = input(str("Enter the color of the fan: "))
 user_power_one = input("Is the fan on/off: ")
 
+# Ask the user for Fan2 Details
+user_speed_two = input("Enter the speed of the fan: ")
+user_radius_two = input(int("Enter the radius of the fan: "))
+user_color_two = input(str("Enter the color of the fan: "))
+user_power_two = input("Is the fan on/off: ")
+
 # Call the methods in the main file
 # Display the Output
 
