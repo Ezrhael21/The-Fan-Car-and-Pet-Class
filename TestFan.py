7a1ef931e666f1887b5bb5f8397a80d9948a11d5
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 00:27:21 +0800

    Ask the user for Fan1 Details

--- TestFan.py
+++ TestFan.py
@@ -6,6 +6,13 @@
 # Create two seperate instances of the Fan Class
 fan_one = ()
 fan_two = ()
+
+# Ask the user for Fan1 Details
+user_speed_one = input("Enter the speed of the fan: ")
+user_radius_one = input(int("Enter the radius of the fan: "))
+user_color_one = input(str("Enter the color of the fan: "))
+user_power_one = input("Is the fan on/off: ")
+
 # Call the methods in the main file
 # Display the Output
 
