c820663382807b785f92deb6c717c93a434fe2f0
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-03 16:19:53 +0800

    Create a class and Initialize the object

---
+++ Fan.py
@@ -0,0 +1,10 @@
+# Create a class named Fan
+class Fan:
+    # Initialize the fan object
+    def __init__(self, speed = 3, radius = 5, color = "blue", power = False):
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.power = power
+    
+    
