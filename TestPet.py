fb9d25e7c7cdde35389430cc1e7ea2b9e2184ed0
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 01:32:50 +0800

    Set the pet details

--- TestPet.py
+++ TestPet.py
@@ -10,9 +10,13 @@ pet = Pet()
 
 # Ask user for pet details
 user_pet_name = input("What is the name of your pet: ")
-user_type_name = input("What is the type of animal your pet is: ")
-user_age_name = int(input("What is the age of your pet: "))
+user_pet_type = input("What is the type of animal your pet is: ")
+user_pet_age = int(input("What is the age of your pet: "))
 
 # Set the pet details
+pet.set_name(user_pet_name)
+pet.set_type(user_pet_type)
+pet.set_age(user_pet_age)
+
 # Display the output
 
