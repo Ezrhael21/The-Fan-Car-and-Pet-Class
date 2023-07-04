63630dbf91cea7cd0f019a203699c32bde5da55d
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 02:13:15 +0800

    Import Colorama

--- TestPet.py
+++ TestPet.py
@@ -4,11 +4,12 @@
 # The Pet Class
 
 import pyfiglet
+import colorama
 
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 font = pyfiglet.figlet_format("Test Pet Program", font = "slant", justify = "center")
-print (font)
-print ("=" * 100)
+print (colorama.Fore.YELLOW + font)
+print (colorama.Fore.GREEN + "=" * 100)
 
 # Call the Pet Class
 from Pet import Pet
@@ -16,10 +17,10 @@ from Pet import Pet
 pet = Pet()
 
 # Ask user for pet details
-user_pet_name = input("What is the name of your pet: ")
+user_pet_name = input(colorama.Fore.BLUE + "What is the name of your pet: ")
 user_pet_type = input("What is the type of animal your pet is: ")
 user_pet_age = int(input("What is the age of your pet: "))
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 
 # Set the pet details
 pet.set_name(user_pet_name)
@@ -27,11 +28,10 @@ pet.set_type(user_pet_type)
 pet.set_age(user_pet_age)
 
 # Display the output
-print ("Pet Details:")
-print ("=" * 100)
-print ("Name: " + str(pet.get_name()))
+print (colorama.Fore.YELLOW + "Pet Details:")
+print (colorama.Fore.WHITE + "Name: " + str(pet.get_name()))
 print ("Type: " + str(pet.get_type()))
 print ("Age: " + str(pet.get_age()))
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 
 
