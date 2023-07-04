430630642a488839716fba6c57da8981386ec95f
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 01:11:26 +0800

    Create a class named Pet & Initialize the pet object

---
+++ Pet.py
@@ -0,0 +1,10 @@
+# Create a class named Pet
+class Pet:
+    # Initialize the pet object
+    def __init__(self, name, type, age):
+        self.__name = name
+        self.__type = type
+        self.__age = age
+    
+
+    
