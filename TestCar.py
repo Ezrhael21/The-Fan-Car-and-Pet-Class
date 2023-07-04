07403e4537398c68b8125737668c2881adc9bc42
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 00:32:14 +0800

    Add Borders

--- TestCar.py
+++ TestCar.py
@@ -5,8 +5,10 @@
 
 import pyfiglet
 
+print ("=" * 100)
 font = pyfiglet.figlet_format("Test Car Program", font = "slant", justify = "center")
 print (font)
+print ("=" * 100)
 
 # Call the Car Class
 from Car import Car
@@ -14,19 +16,26 @@ from Car import Car
 # Ask User for Car Details
 user_car_model = str(input("What is the Year Model of your car: "))
 user_car_make = str(input("What is the Make of your Car: "))
+print ("=" * 100)
 
 car = Car(user_car_model, user_car_make)
 
 # Call the Method Accelerate five times
+print ("Car Accelerate")
+print ("=" * 100)
 for _ in range(5):
     car.accelerate()
     # Display the Output for Car Speed
-    print (str(car.get_speed()) + "kph")
+    print (str(car.get_speed()) + " kph")
+print ("=" * 100)
 
 # Call the Method Brake five times 
+print ("Car Brake")
+print ("=" * 100)
 for _ in range(5):
     car.brake()
     # Display the Output for Car Speed
-    print (str(car.get_speed()) + "kph")
+    print (str(car.get_speed()) + " kph")
+print ("=" * 100)
     
     
