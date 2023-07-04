ae29b1b2fde23b812bb933b3d74ccf4c86849919
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 08:51:44 +0800

    Modify the output display

--- TestFan.py
+++ TestFan.py
@@ -12,8 +12,8 @@ fan_two = Fan()
 
 # Ask the user for Fan1 Details
 user_speed_one = input("Enter the speed of the fan: ")
-user_radius_one = input(int("Enter the radius of the fan: "))
-user_color_one = input(str("Enter the color of the fan: "))
+user_radius_one = input("Enter the radius of the fan: ")
+user_color_one = input("Enter the color of the fan: ")
 user_power_one = input("Is the fan on/off: ")
 
 # Set Fan1 Details 
@@ -23,8 +23,8 @@ fan_one.set_color(user_color_one)
 
 # Ask the user for Fan2 Details
 user_speed_two = input("Enter the speed of the fan: ")
-user_radius_two = input(int("Enter the radius of the fan: "))
-user_color_two = input(str("Enter the color of the fan: "))
+user_radius_two = input("Enter the radius of the fan: ")
+user_color_two = input("Enter the color of the fan: ")
 user_power_two = input("Is the fan on/off: ")
 
 # Set Fan2 Details 
@@ -32,7 +32,13 @@ fan_two.set_speed(user_speed_two)
 fan_two.set_radius(user_radius_two)
 fan_two.set_color(user_color_two)
 
-# Display the Output
-print ("Fan 1 Details:\n Speed:\n" + str(fan_one.get_speed()) + "Radius:\n" + str(fan_one.get_radius()) + "Color:\n" + str(fan_one.get_color()) + "Power:\n" + fan_one.turn_on())
+# Display the Output 
+print ("Fan 1 Details:")
+print ("Speed: " + str(fan_one.get_speed()))
+print ("Radius: " + str(fan_one.get_radius()))
+print ("Color: " + str(fan_one.get_color()))
+print ("Power: " + str(fan_one.turn_on()))
+
+
 
 
