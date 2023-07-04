cdede9267caa5281e51b05ece49cee61a9f8c3c9
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-04 23:30:12 +0800

    Psuedocode for Car class

--- Car.py
+++
@@ -1,4 +0,0 @@
-# Ezrhael R. Sicat
-# BSCpE 1-5
-# 03/07/2023
-# The Car Class
---
+++ TestCar.py
@@ -0,0 +1,13 @@
+# Ezrhael R. Sicat
+# BSCpE 1-5
+# 03/07/2023
+# The Car Class
+
+# Pseudocode
+# Create a class named Car
+# Initialize the car object
+# Method to accelerate the current speed of the car
+# Method to brake the current speed of the car
+# Method to get the current speed of the car
+# Call the Car Class
+# Display the Output
