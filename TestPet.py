6b354d7a151182fbaa0f774404cbe3831fae1b1f
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 02:11:08 +0800

    Add Borders

--- TestPet.py
+++ TestPet.py
@@ -5,8 +5,10 @@
 
 import pyfiglet
 
+print ("=" * 100)
 font = pyfiglet.figlet_format("Test Pet Program", font = "slant", justify = "center")
 print (font)
+print ("=" * 100)
 
 # Call the Pet Class
 from Pet import Pet
@@ -17,17 +19,19 @@ pet = Pet()
 user_pet_name = input("What is the name of your pet: ")
 user_pet_type = input("What is the type of animal your pet is: ")
 user_pet_age = int(input("What is the age of your pet: "))
+print ("=" * 100)
 
 # Set the pet details
 pet.set_name(user_pet_name)
 pet.set_type(user_pet_type)
 pet.set_age(user_pet_age)
 
-
 # Display the output
 print ("Pet Details:")
+print ("=" * 100)
 print ("Name: " + str(pet.get_name()))
 print ("Type: " + str(pet.get_type()))
 print ("Age: " + str(pet.get_age()))
+print ("=" * 100)
 
 
