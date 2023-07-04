8d8105bb3481b344741374b75e8194827cd94214
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 00:41:54 +0800

    Import time delay

--- TestCar.py
+++ TestCar.py
@@ -21,18 +21,28 @@ print (colorama.Fore.GREEN + "=" * 100)
 
 car = Car(user_car_model, user_car_make)
 
-# Call the Method Accelerate five times
+# Time Delay
 print (colorama.Fore.YELLOW + "Car Accelerate")
+print (colorama.Fore.WHITE + "Processing...")
 print (colorama.Fore.GREEN + "=" * 100)
+import time
+time.sleep(5)
+
+# Call the Method Accelerate five times
 for _ in range(5):
     car.accelerate()
     # Display the Output for Car Speed
     print (colorama.Fore.WHITE + str(car.get_speed()) + " kph")
 print (colorama.Fore.GREEN + "=" * 100)
 
-# Call the Method Brake five times 
+# Time Delay
 print (colorama.Fore.YELLOW + "Car Brake")
+print (colorama.Fore.WHITE + "Processing...")
 print (colorama.Fore.GREEN + "=" * 100)
+import time
+time.sleep(5)
+
+# Call the Method Brake five times
 for _ in range(5):
     car.brake()
     # Display the Output for Car Speed
