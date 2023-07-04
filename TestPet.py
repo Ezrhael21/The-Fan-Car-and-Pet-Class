2c2214307c588cafa9e313e60587507d641254ac
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 01:07:19 +0800

    Pseudocode for Pet Class'

--- TestPet.py
+++ TestPet.py
@@ -3,3 +3,17 @@
 # 05/07/2023
 # The Pet Class
 
+# Pseudocode
+# Create a class named Pet
+# Initialize the pet object
+# Method to set the name of the pet
+# Method to set the type of animal the pet is
+# Method to set the age of the pet
+# Method to get the name of the pet
+# Method to get the type of animal the pet is
+# Method to get the age of the pet
+# Call the Pet Class
+# Ask user for pet details
+# Set the pet details
+# Display the output
+
