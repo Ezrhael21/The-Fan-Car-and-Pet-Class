83492db8c34b1efb118fca2f9d6f252d2d6392ea
Ezrhael21 <ezrhaelsicat21@gmail.com>
2023-07-05 00:37:17 +0800

    Import Colorama

--- TestCar.py
+++ TestCar.py
@@ -4,38 +4,39 @@
 # The Car Class
 
 import pyfiglet
+import colorama
 
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 font = pyfiglet.figlet_format("Test Car Program", font = "slant", justify = "center")
-print (font)
-print ("=" * 100)
+print (colorama.Fore.YELLOW + font)
+print (colorama.Fore.GREEN + "=" * 100)
 
 # Call the Car Class
 from Car import Car
 
 # Ask User for Car Details
-user_car_model = str(input("What is the Year Model of your car: "))
+user_car_model = str(input(colorama.Fore.BLUE + "What is the Year Model of your car: "))
 user_car_make = str(input("What is the Make of your Car: "))
-print ("=" * 100)
+print (colorama.Fore.GREEN + "=" * 100)
 
 car = Car(user_car_model, user_car_make)
 
 # Call the Method Accelerate five times
-print ("Car Accelerate")
-print ("=" * 100)
+print (colorama.Fore.YELLOW + "Car Accelerate")
+print (colorama.Fore.GREEN + "=" * 100)
 for _ in range(5):
     car.accelerate()
     # Display the Output for Car Speed
-    print (str(car.get_speed()) + " kph")
-print ("=" * 100)
+    print (colorama.Fore.WHITE + str(car.get_speed()) + " kph")
+print (colorama.Fore.GREEN + "=" * 100)
 
 # Call the Method Brake five times 
-print ("Car Brake")
-print ("=" * 100)
+print (colorama.Fore.YELLOW + "Car Brake")
+print (colorama.Fore.GREEN + "=" * 100)
 for _ in range(5):
     car.brake()
     # Display the Output for Car Speed
-    print (str(car.get_speed()) + " kph")
-print ("=" * 100)
+    print (colorama.Fore.WHITE + str(car.get_speed()) + " kph")
+print (colorama.Fore.GREEN + "=" * 100)
     
     
